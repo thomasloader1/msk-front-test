b89ad059a86369be5e452b75e819666ed611ed0d
Luciano Castro <luciano.castro@outlook.com>
2023-05-02 12:09:55 -0300

    Added new logos

--- src/contains/brands.ts
+++ src/contains/brands.ts
@@ -1,11 +1,11 @@
 import brand1 from "images/brands/tropos.svg";
-import brand2 from "images/brands/acp.svg";
+import brand2 from "images/brands/innovamos_home.png";
 import brand3 from "images/brands/acc.svg";
-import brand4 from "images/brands/mayo.svg";
+import brand4 from "images/brands/om_home.png";
 
 export const brands = [
   { img: brand1, width: 200 },
   { img: brand2, width: 250 },
   { img: brand3, width: 200 },
-  { img: brand4, width: 70 },
+  { img: brand4, width: 120 },
 ];

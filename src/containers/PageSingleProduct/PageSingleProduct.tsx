6a9abf529a61b0ad5a851e7517ae55b857fe64ec
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-08-09 14:46:02 -0300

    Agrego hook de useSingleProdcut

--- src/containers/PageSingleProduct/PageSingleProduct.tsx
+++ src/containers/PageSingleProduct/PageSingleProduct.tsx
@@ -1,38 +1,19 @@
 import SingleProductDetail from "components/SingleProductDetail/SingleProductDetail";
-import React, { useContext, useEffect, useState } from "react";
-import { HOME_COURSES } from "data/MSK/courses";
-import { FetchSingleProduct } from "data/types";
-import axios from "axios";
+import React, { useContext } from "react";
 import LoadingText from "components/Loader/Text";
-import { API_URL } from "data/api";
 import { CountryContext } from "context/country/CountryContext";
+import useSingleProduct from "hooks/useSingleProduct";
 
 const PageSingleProduct = () => {
   const { state } = useContext(CountryContext);
 
-  const slug = window.location.href.split("/").pop();
-  const [product, setProduct] = useState<FetchSingleProduct>();
-  const [isLoading, setLoading] = useState(false);
+  const slug = window.location.href.split("/").pop() || '';
+  const { product, loading } = useSingleProduct(slug, { country: state.country });
 
-  useEffect(() => {
-    setLoading(true);
-    if (slug) {
-      axios
-        .get(`${API_URL}/product/${slug}?country=${state.country}`)
-        .then((response) => {
-          setLoading(false);
-          setProduct(response.data);
-        })
-        .catch((error) => {
-          setLoading(false);
-          console.log(error);
-        });
-    }
-  }, []);
   return (
     <div className={`nc-PageSubcription `} data-nc-id="PageSubcription">
       <section className="text-neutral-600 text-sm md:text-base overflow-hidden">
-        {isLoading ? (
+        {loading ? (
           <div className="container grid gap-16 py-16">
             <LoadingText />
             <LoadingText />
---
+++ src/hooks/useSingleProduct.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from "data/api";
+import { FetchSingleProduct } from 'data/types';
+
+const useSingleProduct = (slug: string, state: { country: string }) => {
+    const [product, setProduct] = useState<FetchSingleProduct>();
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchProduct = async () => {
+        try {
+            const response = await axios.get(`${API_URL}/product/${slug}?country=${state.country}`)
+            setProduct(response.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
+    useEffect(() => {
+        setLoading(true);
+        if (slug) {
+            fetchProduct();
+        }
+    }, [slug, state.country]);
+
+    return { product, loading }
+};
+
+export default useSingleProduct;

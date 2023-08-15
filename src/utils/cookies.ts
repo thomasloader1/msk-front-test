78b1fa78050893a7e1c389a0b2ace1fa5dfffe7c
lucianoNerdStuds <luciano@nerdstuds.com>
2023-08-15 17:58:44 -0300

    Forms UTMs logic updated

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -24,4 +24,9 @@ export const setCookie = (name: string, value: string, days: number) => {
 export const deleteCookie = (name: string) => {
   console.log("deleteCookie", name);
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  let input = document.querySelector('input[type="hidden"][name="'+name+'"]');
+  if (input) {
+    // @ts-ignore
+    input.value = "";
+  }
 };

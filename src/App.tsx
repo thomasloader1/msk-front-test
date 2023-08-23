4a1e1f30333cfae56c29ba53c8dea7f0e8691ab8
flidas-oceano <talk.tomas@hotmail.com>
2023-08-23 17:00:19 -0300

    Quieto peticion erronea de categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { StoreFiltersProvider } from "context/storeFilters/StoreFiltersProvider";
 import AuthProvider from "context/user/AuthProvider";
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import MyRouter from "routers";
 import { CountryProvider } from "context/country/CountryProvider";
-import queryString from "query-string";
 import UTMProvider from "context/utm/UTMProvider";
 
 function App() {
-  useEffect(() => {
-    const { VITE_OM_WP_API } = import.meta.env;
-    const fetchData = async () => {
-      const request = await fetch(`${VITE_OM_WP_API}/categories`);
-      const data = await request.json();
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">

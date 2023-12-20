a252dfacce62e82ee36f35bb362956c26f60853f
lucianoNerdStuds <luciano@nerdstuds.com>
2023-12-20 13:45:27 -0300

    update prod path

--- src/data/api.ts
+++ src/data/api.ts
@@ -4,7 +4,7 @@ const { VITE_MSK_WP_API, VITE_PUBLIC_URL, PROD, VITE_PUBLIC_URL_DEV } =
   import.meta.env;
 const API_URL = VITE_MSK_WP_API;
 const API_BACKEND_LARAVEL = PROD
-  ? `${VITE_PUBLIC_URL}/msk-laravel/public/api`
+  ? `${VITE_PUBLIC_URL}/api`
   : `${VITE_PUBLIC_URL_DEV}/api`;
 
 const baseUrl = PROD

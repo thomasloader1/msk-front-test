6bcf21d5f2975c0b2afb4137f5e3355d9af1416c
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2024-01-30 10:33:01 -0300

    Order brands

--- src/contains/brands.ts
+++ src/contains/brands.ts
@@ -8,6 +8,6 @@ export const brands = [
   { img: brand1, width: 200 },
   { img: brand2, width: 350 },
   { img: brand3, width: 200 },
-  { img: brand4cl, width: 150, country: "cl" },
   { img: brand5cl, width: 200, country: "cl" },
+  { img: brand4cl, width: 150, country: "cl" },
 ];

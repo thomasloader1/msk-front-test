a16bf36e0023fade649135eb9c8817de34b94b3c
Tomas Gonzalo Gomez <tomasgomez@msklatam.com>
2024-03-14 12:53:58 -0300

    Link suggest note

--- src/components/NoteExtraData/NoteExtraData.tsx
+++ src/components/NoteExtraData/NoteExtraData.tsx
@@ -23,6 +23,10 @@ const Excerpt: React.FC<NoteExtraDataProps> = ({
 
   const showSuggestContent = suggest_content?.description && suggest_content.description != '';
 
+const isEndpoint = suggest_content?.link.url.startsWith('/');
+const url = isEndpoint
+  ? `${window.location.origin}${suggest_content?.link.url}`
+  : suggest_content?.link.url;
   return (
     <>
       {excerpt && (
@@ -40,7 +44,7 @@ const Excerpt: React.FC<NoteExtraDataProps> = ({
       {showSuggestContent && (
         <div className="my-10">
           <div dangerouslySetInnerHTML={{ __html: suggest_content?.description as string }} className="mb-7 font-lora text-slate-500 text-xl" />
-          <Link to={suggest_content?.link.url as string} className="nc-Button  transition-colors rounded text-sm sm:text-base px-4 py-2 sm:px-5  ttnc-ButtonPrimary bg-primary-6000 text-neutral-50 hover:bg-red-500 hover:text-neutral-50 font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0 no-underline" target={suggest_content.link.target}>{suggest_content?.link.title}</Link>
+          <a href={url}  className="nc-Button  transition-colors rounded text-sm sm:text-base px-4 py-2 sm:px-5  ttnc-ButtonPrimary bg-primary-6000 text-neutral-50 hover:bg-red-500 hover:text-neutral-50 font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0 no-underline" target={suggest_content.link.target}>{suggest_content?.link.title}</a>
         </div>
       )}
     </>

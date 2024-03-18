b8533a1913bc5f3e14c43c59b63cb10ac2302ac8
lucianoNerdStuds <luciano@nerdstuds.com>
2024-03-18 19:40:43 -0300

    Test cloudflare

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   //output: 'export', //For static site (loses SSR cappabilites)
   //distDir: "dist",
 
+  experimental: {
+    runtime: 'experimental-edge',
+  },
+
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if

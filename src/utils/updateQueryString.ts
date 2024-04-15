b23d53f5f5eac65fe8fef2522fccb2aa7b5d267f
Tomas <talk.gtg@gmail.com>
2024-04-15 17:45:26 -0300

    add updateQueryString.ts

---
+++ src/utils/updateQueryString.ts
@@ -0,0 +1,19 @@
+type newQueryTerm ={
+    key: string;
+    value: string;
+}
+
+export function updateQueryString(endpoint: string,queryString: string, newQueryString: newQueryTerm) {
+    const searchParams = new URLSearchParams(queryString);
+    
+        if (searchParams.has(newQueryString.key)) {
+            searchParams.set(newQueryString.key, newQueryString.value);
+          } else {
+            searchParams.append(newQueryString.key, newQueryString.value);
+          }
+        
+          return `${endpoint}?${searchParams.toString()}`;
+   
+  }
+
+  

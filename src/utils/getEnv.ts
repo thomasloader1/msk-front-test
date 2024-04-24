bbb2d2179fc74720e17cb2518d8de9fec880cf96
Tomas Gonzalo Gomez <tomasgomez@msklatam.com>
2024-04-24 16:51:53 -0300

    Manage env

--- src/utils/getEnv.ts
+++ src/utils/getEnv.ts
@@ -1,12 +1,12 @@
 import { REBILL_CONF } from "@/logic/Rebill";
 
 export const getEnv = (name: string) => {
-  const { PROD } = process.env;
-  const isProd = PROD ? "PRD" : "TEST";
+  const { PROD,NODE_ENV } = process.env;
+  const isProd = (PROD || NODE_ENV === 'production') ? "PRD" : "TEST";
   const [first, ...rest] = name.split("_");
   const envVariable = `NEXT_PUBLIC_${first}_${rest.join("_")}_${isProd}`;
   // @ts-ignore
-  console.log({envVariable},REBILL_CONF.PRICES[envVariable])
+  console.log({PROD, NODE_ENV,envVariable},REBILL_CONF.PRICES[envVariable])
   // @ts-ignore
   return REBILL_CONF.PRICES[envVariable];
 };

1452e792f2f9a8c66baf9d81c28238bd795c7a23
lucianoNerdStuds <luciano@nerdstuds.com>
2024-06-17 23:20:20 -0300

    Componente gracias

--- src/app/[lang]/gracias/page.tsx
+++ src/app/[lang]/gracias/page.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
-import PageGraciasComponent from "@/components/MSK/crear-cuenta/Page";
+import PageGraciasComponent from "@/components/MSK/gracias/Page";
 export async function generateMetadata() {
   return {
     title: "Gracias",
   };
 }
 const PageGracias: FC = () => {
-  return <PageGraciasComponent/>;
+  return <PageGraciasComponent className={""}/>;
 };
 
 export default PageGracias;
